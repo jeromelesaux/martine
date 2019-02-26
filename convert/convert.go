15fb5e0a1f756be108f76084dbabc1a574297810
Jerome Le Saux <j.lesaux@axione.fr>
2019-02-26 13:52:22 +0100

    add: add resized and downgrade traitments.

--- convert/convert.go
+++ convert/convert.go
@@ -1,15 +1,57 @@
 package convert
 
 import (
-	"image"
 	"fmt"
-	"os"
-	"github.com/jeromelesaux/screenverter/gfx"
 	"github.com/disintegration/imaging"
+	"github.com/jeromelesaux/screenverter/gfx"
+	"image"
+	"image/color"
+	"os"
 )
 
-func Resize(in image.Image,size gfx.Size) *image.NRGBA {
-	fmt.Fprintf(os.Stdout,"Resizing image to width %d pixels heigh %d\n",size.Width,size.Height)
-	return  imaging.Resize(in,size.Width,size.Height,imaging.Lanczos)
+func Resize(in image.Image, size gfx.Size) *image.NRGBA {
+	fmt.Fprintf(os.Stdout, "* Step 1 * Resizing image to width %d pixels heigh %d\n", size.Width, size.Height)
+	return imaging.Resize(in, size.Width, size.Height, imaging.Lanczos)
 }
 
+func DowngradingPalette(in *image.NRGBA, size gfx.Size)  *image.NRGBA {
+	p,out := downgrade(in)
+	fmt.Fprintf(os.Stdout,"Current palette contains (%d) colors\n",len(p))
+	if len(p) > size.ColorsAvailable {
+		fmt.Fprintf(os.Stderr,"Current palette size (%d) is greater than the available colors in this mode (%d)\n",len(p),size.ColorsAvailable)
+		phasis := 1
+		for {
+			fmt.Fprintf(os.Stderr,"Phasis (%d) downgrade colors palette\n",phasis)
+			p, out = downgrade(out)
+			if len(p) <= size.ColorsAvailable {
+				break
+			}
+			phasis++
+		}
+	}
+	return out
+}
+
+func downgrade(in *image.NRGBA) (color.Palette,*image.NRGBA) {
+	p := color.Palette{}
+	for y := in.Bounds().Min.Y; y < in.Bounds().Max.Y; y++ {
+		for x := in.Bounds().Min.X; x < in.Bounds().Max.X; x++ {
+			c := in.At(x, y)
+			cPalette := gfx.CpcOldPalette.Convert(c)
+			in.Set(x,y,cPalette)
+			if !paletteContains(p, cPalette) {
+				p = append(p, cPalette)
+			}
+		}
+	}
+	return p, in
+}
+
+func paletteContains(p color.Palette, c color.Color) bool {
+	for _, cp := range p {
+		if cp == c {
+			return true
+		}
+	}
+	return false
+}
--- main.go
+++ main.go
@@ -1,26 +1,29 @@
-package main 
+package main
 
 import (
-	"fmt"
 	"flag"
+	"fmt"
+	"github.com/jeromelesaux/screenverter/convert"
+	"github.com/jeromelesaux/screenverter/gfx"
+	"image"
+	_ "image/jpeg"
+	"image/png"
 	"os"
 	"strings"
-	"github.com/jeromelesaux/screenverter/gfx"
 )
 
-
 var (
-	picturePath = flag.String("p","","Picture path of the Amsdos file.")
-	width  = flag.Int("w",-1,"Custom output width in pixels.")
-	height = flag.Int("h",-1,"Custom output height in pixels.")
-	mode = flag.String("m","","Output mode to use (mode0,mode1,mode2 or overscan available).")
-	output = flag.String("o","","Output directory")
+	picturePath = flag.String("p", "", "Picture path of the Amsdos file.")
+	width       = flag.Int("w", -1, "Custom output width in pixels.")
+	height      = flag.Int("h", -1, "Custom output height in pixels.")
+	mode        = flag.String("m", "", "Output mode to use (mode0,mode1,mode2 or overscan available).")
+	output      = flag.String("o", "", "Output directory")
 )
 
 func main() {
-	 var size gfx.Size
+	var size gfx.Size
 	flag.Parse()
-	// picture path to convert 
+	// picture path to convert
 	if *picturePath == "" {
 		flag.PrintDefaults()
 		os.Exit(-1)
@@ -29,36 +32,81 @@ func main() {
 	if *output != "" {
 		fi, err := os.Stat(*output)
 		if err != nil {
-			fmt.Fprintf(os.Stderr,"Error while getting directory informations :%v, Quiting\n",err)
+			fmt.Fprintf(os.Stderr, "Error while getting directory informations :%v, Quiting\n", err)
 			os.Exit(-2)
 		}
-		
+
 		if !fi.IsDir() {
-			fmt.Fprintf(os.Stderr,"%s is not a directory will store in current directory\n",*output)
+			fmt.Fprintf(os.Stderr, "%s is not a directory will store in current directory\n", *output)
 			*output = "./"
 		}
 	}
 	if *height != -1 && *width != -1 {
-		fmt.Fprintf(os.Stderr,"Use the custom informations (width:%d, height:%d)\n",*width,*height)
+		fmt.Fprintf(os.Stderr, "Use the custom informations (width:%d, height:%d)\n", *width, *height)
 		size.Height = *height
 		size.Width = *width
 	} else {
 		if *mode == "" {
-			fmt.Fprintf(os.Stderr,"No output mode defined can not choose. Quiting\n")
+			fmt.Fprintf(os.Stderr, "No output mode defined can not choose. Quiting\n")
 			flag.PrintDefaults()
 			os.Exit(-2)
 		}
 		switch strings.ToLower(*mode) {
-		case "mode0": size = gfx.Mode0
-		case "mode1": size = gfx.Mode1
-		case "mode2": size = gfx.Mode2
-		case "overscan": size = gfx.Overscan
-		default: fmt.Fprintf(os.Stderr,"mode %s not defined\n", *mode) 
-		flag.PrintDefaults()
-		os.Exit(-2)
+		case "mode0":
+			size = gfx.Mode0
+		case "mode1":
+			size = gfx.Mode1
+		case "mode2":
+			size = gfx.Mode2
+		case "overscan":
+			size = gfx.Overscan
+		default:
+			fmt.Fprintf(os.Stderr, "mode %s not defined\n", *mode)
+			flag.PrintDefaults()
+			os.Exit(-2)
 		}
 	}
 
-	fmt.Fprintf(os.Stdout,"Informations :\n%s",size.ToString())
+	fmt.Fprintf(os.Stdout, "Informations :\n%s", size.ToString())
+
+	f, err := os.Open(*picturePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error while opening file %s, error %v\n", *picturePath, err)
+		os.Exit(-2)
+	}
+	defer f.Close()
+	in, _, err := image.Decode(f)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Cannot decode the image %s error %v", *picturePath, err)
+		os.Exit(-2)
+	}
+	out := convert.Resize(in, size)
+	fmt.Fprintf(os.Stdout,"Saving resized image into (%s)\n", *picturePath+"_resized.png")
+	fw,err := os.Create(*picturePath+"_resized.jpg")
+	if err != nil {
+		fmt.Fprintf(os.Stderr,"Cannot create new image (%s) error %v\n",*picturePath+"_resized.png",err)
+		os.Exit(-2)
+	}
+	if err := png.Encode(fw,out); err != nil {
+		fmt.Fprintf(os.Stderr,"Cannot create new image (%s) as png error %v\n",*picturePath+"_resized.png",err)
+		fw.Close()
+		os.Exit(-2)
+	}
+	fw.Close()
+	downgraded := convert.DowngradingPalette(out,size)
+	fmt.Fprintf(os.Stdout,"Saving downgraded image into (%s)\n", *picturePath+"_down.png")
+	fw,err = os.Create(*picturePath+"_down.jpg")
+	if err != nil {
+		fw.Close()
+		fmt.Fprintf(os.Stderr,"Cannot create new image (%s) error %v\n",*picturePath+"_down.png",err)
+		os.Exit(-2)
+	}
+	fw.Close()
+	if err := png.Encode(fw,downgraded); err != nil {
+		fw.Close()
+		fmt.Fprintf(os.Stderr,"Cannot create new image (%s) as png error %v\n",*picturePath+"_down.png",err)
+		os.Exit(-2)
+	}
+	
 
 }
