313ac24fbacc50593ce4ded41f90a7381124247f
Jerome Le Saux <jlesaux@free.fr>
2019-03-20 22:43:32 +0100

    fix: fix eol for windows os.

--- gfx/ascii.go
+++ gfx/ascii.go
@@ -7,19 +7,24 @@ import (
 	"image/color"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 )
 
 var ByteToken = "BYTE"
 
 func Ascii(filePath, dirPath string, data []byte, p color.Palette, noAmsdosHeader, isCpcPlus bool) error {
+	eol := "\n"
+	if runtime.GOOS == "windows" {
+		eol = "\r\n"
+	}
 	fmt.Fprintf(os.Stdout, "Writing ascii file (%s) data length (%d)\n", filePath, len(data))
 	var out string
 	var i int
 	filename := filepath.Base(filePath)
 	extension := filepath.Ext(filename)
 	cpcFilename := strings.ToUpper(strings.Replace(filename, extension, ".TXT", -1))
-	out += "; Screen " + cpcFilename + "\n.screen:\n"
+	out += "; Screen " + cpcFilename + eol + ".screen:" + eol
 	for i = 0; i < len(data); i += 8 {
 		out += fmt.Sprintf("%s ", ByteToken)
 		if i < len(data) {
@@ -46,9 +51,9 @@ func Ascii(filePath, dirPath string, data []byte, p color.Palette, noAmsdosHeade
 		if i+7 < len(data) {
 			out += fmt.Sprintf(", #%0.2x", data[i+7])
 		}
-		out += fmt.Sprintf("\n")
+		out += fmt.Sprintf("%s", eol)
 	}
-	out += "; Palette " + cpcFilename + "\n.palette:\n" + ByteToken + " "
+	out += "; Palette " + cpcFilename + eol + ".palette:" + eol + ByteToken + " "
 
 	if isCpcPlus {
 		for i := 0; i < len(p); i++ {
@@ -56,7 +61,7 @@ func Ascii(filePath, dirPath string, data []byte, p color.Palette, noAmsdosHeade
 			v := cp.Value()
 			out += fmt.Sprintf("#%.2x, #%.2x", byte(v), byte(v>>8))
 			if (i+1)%8 == 0 && i+1 < len(p) {
-				out += "\n" + ByteToken + " "
+				out += eol + ByteToken + " "
 			} else {
 				if i+1 < len(p) {
 					out += ", "
@@ -69,7 +74,7 @@ func Ascii(filePath, dirPath string, data []byte, p color.Palette, noAmsdosHeade
 			if err == nil {
 				out += fmt.Sprintf("#%0.2x", v[0])
 				if (i+1)%8 == 0 && i+1 < len(p) {
-					out += "\n" + ByteToken + " "
+					out += eol + ByteToken + " "
 				} else {
 					if i+1 < len(p) {
 						out += ", "
@@ -79,13 +84,13 @@ func Ascii(filePath, dirPath string, data []byte, p color.Palette, noAmsdosHeade
 				fmt.Fprintf(os.Stderr, "Error while getting the hardware values for color %v, error :%d\n", p[0], err)
 			}
 		}
-		out += "\n; Basic Palette " + cpcFilename + "\n.basic_palette:\n#" + ByteToken + " "
+		out += eol + "; Basic Palette " + cpcFilename + eol + ".basic_palette:" + eol + "#" + ByteToken + " "
 		for i := 0; i < len(p); i++ {
 			v, err := FirmwareNumber(p[i])
 			if err == nil {
 				out += fmt.Sprintf("%0.2d", v)
 				if (i+1)%8 == 0 && i+1 < len(p) {
-					out += "\n#" + ByteToken + " "
+					out += eol + "#" + ByteToken + " "
 				} else {
 					if i+1 < len(p) {
 						out += ", "
@@ -95,7 +100,7 @@ func Ascii(filePath, dirPath string, data []byte, p color.Palette, noAmsdosHeade
 				fmt.Fprintf(os.Stderr, "Error while getting the hardware values for color %v, error :%d\n", p[0], err)
 			}
 		}
-		out += "\n"
+		out += eol
 	}
 	//fmt.Fprintf(os.Stdout,"%s",out)
 	header := cpc.CpcHead{Type: 0, User: 0, Address: 0x0, Exec: 0x0,
