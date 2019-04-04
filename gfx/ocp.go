0f4a0b0f1f100fa359fb2ed7a27fb0c779881ee9
Jerome Le Saux <jlesaux@free.fr>
2019-04-04 23:25:34 +0200

    add: add value from file to rgb Cpc Plus structure.

--- gfx/ocp.go
+++ gfx/ocp.go
@@ -32,23 +32,21 @@ type CpcPlusColor struct {
 
 func NewRawCpcPlusColor( v uint16) *CpcPlusColor {
 	c := &CpcPlusColor{}
-	c.B = v & 0xf   //1111
-	c.R = (v - c.B) / 16
-	c.G = v - c.B / 256
-	
-	
+	c.B = v  & 0xf//1111
+	c.R = v >> 4 & 0xf
+	c.G = v >> 8 & 0xf
 	return c
 }
 
 func (c *CpcPlusColor)ToString() string {
-	return fmt.Sprintf("R:%.2b,G:%.2b,B:%.2b",c.R,c.G,c.B)
+	return fmt.Sprintf("R:%.2b(%d),G:%.2b(%d),B:%.2b(%d)",c.R,c.R,c.G,c.G,c.B,c.B)
 }
 
 func (c *CpcPlusColor) Value() uint16 {
-	fmt.Fprintf(os.Stderr, "value(%d,%d,%d) #%.4x : %d\n", c.R, c.G, c.B,
-		c.B|c.R<<4|c.G<<8,
-		c.B+c.R*16+c.G*256)
-	return uint16(c.B + c.R*16 + c.G*256)
+	v := c.B | c.R<<4 | c.G <<8
+	fmt.Fprintf(os.Stderr, "value(%d,%d,%d)(%b,%b,%b) #%.4x (%.b): %d\n", c.R, c.G, c.B,c.R, c.G, c.B,
+	v,v,c.B+(c.R*16)+c.G*256)
+	return v
 }
 func (c *CpcPlusColor) Bytes() []byte {
 	buf := make([]byte, 2)
--- gfx/ocp_test.go
+++ gfx/ocp_test.go
@@ -5,8 +5,9 @@ import (
 )
 
 func TestCpcPlusColor(t *testing.T) {
-	c1 := CpcPlusColor{G:15,R:200,B:6}
+	c1 := CpcPlusColor{G:0xa,R:0xf,B:0xf}
 	v := c1.Value()
 	c2 := NewRawCpcPlusColor(v)
+	t.Logf("%b,%b\n",0xFF0000,v >> 8)
 	t.Logf("\nc1:%s\nc2:%s\nv:%b\n",c1.ToString(),c2.ToString(),v)
 }
