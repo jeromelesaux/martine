504f2af62ef2ec9d41dd4f5cedc3aa9955742af8
Jerome Le Saux <jeromelesaux@gmail.com>
2019-04-08 16:50:35 +0200

    fix: fix rotate by keeping image ratio.

--- gfx/rotate.go
+++ gfx/rotate.go
@@ -16,34 +16,55 @@ var (
 	ErrorSizeMismatch                   = errors.New("Error width and height mismatch cannot perform rotation.")
 )
 
-func Rotate(in *image.NRGBA, p color.Palette, size constants.Size, mode uint8, filePath string, resizeAlgo imaging.ResampleFilter,exportType *ExportType) error {
+func Rotate(in *image.NRGBA, p color.Palette, size constants.Size, mode uint8, filePath string, resizeAlgo imaging.ResampleFilter, exportType *ExportType) error {
 	if exportType.RollIteration == -1 {
 		return ErrorMissingNumberOfImageToGenerate
 	}
-	maxSize := constants.Size{}
 	if size.Width != size.Height {
 		return ErrorSizeMismatch
 	}
 	var indice int
 	angle := 360. / float64(exportType.RollIteration)
-	for i := 0. ; i < 360.; i += angle {
-		rin := imaging.Rotate(in, float64(i), color.Transparent)
-		if maxSize.Width < rin.Bounds().Max.X || maxSize.Height < rin.Bounds().Max.Y {
-			maxSize.Width = rin.Bounds().Max.X 
-			maxSize.Height = rin.Bounds().Max.Y
-		}
-	}
-
-	fmt.Fprintf(os.Stdout,"initiale size (%d,%d) maxsize (%d,%d)\n",size.Width,size.Height,maxSize.Width,maxSize.Height )
 
 	for i := 0.; i < 360.; i += angle {
-		rin := imaging.Rotate(in, float64(i), color.Transparent)
-		rin = convert.Resize(rin,size,resizeAlgo)
-		newFilename := exportType.OsFullPath(filePath,fmt.Sprintf("%.2d",indice)+ ".png")
+		rin := imaging.Rotate(in, float64(i), color.Black)
+
+
+		if rin.Bounds().Max.X%2 != 0 {
+			newSize := constants.Size{
+				ColorsAvailable: size.ColorsAvailable,
+				ColumnsNumber:   size.ColumnsNumber,
+				LinesNumber:     size.LinesNumber,
+				Width:           rin.Bounds().Max.X + 1,
+				Height:          rin.Bounds().Max.Y + 1,
+			}
+			rin = convert.Resize(rin, newSize, resizeAlgo)
+		} else {
+
+			if rin.Bounds().Max.Y%2 != 0 {
+				newSize := constants.Size{
+					ColorsAvailable: size.ColorsAvailable,
+					ColumnsNumber:   size.ColumnsNumber,
+					LinesNumber:     size.LinesNumber,
+					Width:           rin.Bounds().Max.X + 1,
+					Height:          rin.Bounds().Max.Y + 1,
+				}
+				rin = convert.Resize(rin, newSize, resizeAlgo)
+			}
+		}
+		newSize := constants.Size{
+			ColorsAvailable: size.ColorsAvailable,
+			ColumnsNumber:   size.ColumnsNumber,
+			LinesNumber:     size.LinesNumber,
+			Width:           rin.Bounds().Max.X,
+			Height:          rin.Bounds().Max.Y,
+		}
+
+		newFilename := exportType.OsFullPath(filePath, fmt.Sprintf("%.2d", indice)+".png")
 		if err := Png(newFilename, rin); err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot create image (%s) error :%v\n", newFilename, err)
 		}
-		if err := SpriteTransform(rin, p, size, mode, newFilename, exportType); err != nil {
+		if err := SpriteTransform(rin, p, newSize, mode, newFilename, exportType); err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot create sprite image (%s) error %v\n", newFilename, err)
 		}
 		indice++
