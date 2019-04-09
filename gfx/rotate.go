519c135e2af4082047d0a6133985a7a551ed8e42
Jerome Le Saux <jlesaux@free.fr>
2019-04-09 21:21:10 +0200

    add: cleaning code.

--- gfx/rotate.go
+++ gfx/rotate.go
@@ -36,40 +36,13 @@ func Rotate(in *image.NRGBA, p color.Palette, size constants.Size, mode uint8, f
 		rin := imaging.Rotate(in, float64(i), color.Transparent)
 
 		if rin.Bounds().Max.X < maxSize.Width || rin.Bounds().Max.Y < maxSize.Height {
-			//orig := constants.Size{Width:rin.Bounds().Max.X,Height:rin.Bounds().Max.Y}
-			//	rin = convert.Resize(rin, maxSize,resizeAlgo )
 			background := image.NewRGBA(image.Rectangle{image.Point{X: 0, Y: 0}, image.Point{X: maxSize.Width, Y: maxSize.Height}})
-
 			rin = imaging.PasteCenter(
 				background,
 				rin,
 			)
 		}
-		//rin = convert.Resize(rin, maxSize, resizeAlgo)
-
-		/*if rin.Bounds().Max.X%2 != 0 {
-			newSize := constants.Size{
-				ColorsAvailable: size.ColorsAvailable,
-				ColumnsNumber:   size.ColumnsNumber,
-				LinesNumber:     size.LinesNumber,
-				Width:           rin.Bounds().Max.X + 1,
-				Height:          rin.Bounds().Max.Y + 1,
-			}
-			rin = convert.Resize(rin, newSize, resizeAlgo)
-		} else {
-
-			if rin.Bounds().Max.Y%2 != 0 {
-				newSize := constants.Size{
-					ColorsAvailable: size.ColorsAvailable,
-					ColumnsNumber:   size.ColumnsNumber,
-					LinesNumber:     size.LinesNumber,
-					Width:           rin.Bounds().Max.X + 1,
-					Height:          rin.Bounds().Max.Y + 1,
-				}
-				rin = convert.Resize(rin, newSize, resizeAlgo)
-			}
-		}*/
-
+	
 		newFilename := exportType.OsFullPath(filePath, fmt.Sprintf("%.2d", indice)+".png")
 		if err := Png(newFilename, rin); err != nil {
 			fmt.Fprintf(os.Stderr, "Cannot create image (%s) error :%v\n", newFilename, err)
