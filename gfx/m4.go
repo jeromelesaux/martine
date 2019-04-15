e9cc74471ac5f24dc16480b7d661e9dbd594961b
Jerome Le Saux <jeromelesaux@gmail.com>
2019-04-15 11:12:28 +0200

    add: add set remote path on m4 before execute file.

--- gfx/m4.go
+++ gfx/m4.go
@@ -22,6 +22,11 @@ func ImportInM4(exportType *ExportType) error {
 	}
 
 	client := m4.M4Client{IPClient: exportType.M4Host}
+
+	if err := client.MakeDirectory(exportType.M4RemotePath); err != nil {
+		fmt.Fprintf(os.Stderr,"Cannot create directory on M4 (%s) error %v\n",exportType.M4RemotePath$,err)
+	}
+
 	for _, v := range exportType.DskFiles {
 		if err := client.Upload(exportType.M4RemotePath, v); err != nil {
 			fmt.Fprintf(os.Stderr, "Something is wrong M4 host (%s) local file (%s) remote path (%s) error :%v\n",
@@ -43,6 +48,13 @@ func ImportInM4(exportType *ExportType) error {
 	}
 
 	if exportType.M4Autoexec {
+		p, err := client.Ls(exportType.M4RemotePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr,"Cannot go to the remote path (%s) error :%v\n", err)
+		} else {
+			fmt.Fprintf(os.Stdout,"Set the remote path (%s) \n",p)
+		}
+
 		var  overscanFile, basicFile string
 		for _, v := range exportType.DskFiles {
 			switch  path.Ext(v) {
