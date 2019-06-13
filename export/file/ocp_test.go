05857dabf6e77f520ce5b226d5e52bd5a96bd52b
Jerome Le Saux <j.lesaux@axione.fr>
2019-06-13 15:59:03 +0200

    fix: unit tests fixed.

--- export/file/ocp_test.go
+++ export/file/ocp_test.go
@@ -2,12 +2,13 @@ package file
 
 import (
 	"testing"
+	"github.com/jeromelesaux/martine/constants"
 )
 
 func TestCpcPlusColor(t *testing.T) {
-	c1 := CpcPlusColor{G: 0xa, R: 0xf, B: 0xf}
+	c1 := constants.CpcPlusColor{G: 0xa, R: 0xf, B: 0xf}
 	v := c1.Value()
-	c2 := NewRawCpcPlusColor(v)
+	c2 := constants.NewRawCpcPlusColor(v)
 	t.Logf("%b,%b\n", 0xFF0000, v>>8)
 	t.Logf("\nc1:%s\nc2:%s\nv:%b\n", c1.ToString(), c2.ToString(), v)
 }
