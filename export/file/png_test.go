b4cc908af54751f71b6e667debe8cdfff635247a
Jerome Le Saux <jeromelesaux@gmail.com>
2020-03-10 22:50:15 +0100

    add: add png palette used to generate image and unit test.

--- export/file/png_test.go
+++ export/file/png_test.go
@@ -8,6 +8,8 @@ import (
 
 func TestPaletteOutput(t *testing.T) {
 	p := constants.CpcOldPalette
-	PalToPng("test.png", p[0:16])
+	if err := PalToPng("test.png", p[0:16]); err != nil {
+		t.Fatalf("error while generating the palette with error :%v\n", err)
+	}
 
 }
