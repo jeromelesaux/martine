8768fa2326db7510339d2b6d8bb48a3ed0c76bd4
Jerome Le Saux <jeromelesaux@gmail.com>
2021-12-02 22:42:32 +0100

    add: add feature to treat the image.

--- ui/martine-ui/martine_ui.go
+++ ui/martine-ui/martine_ui.go
@@ -18,10 +18,12 @@ import (
 	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+	"github.com/disintegration/imaging"
 	"github.com/jeromelesaux/martine/common"
 	"github.com/jeromelesaux/martine/constants"
 	"github.com/jeromelesaux/martine/export"
 	"github.com/jeromelesaux/martine/gfx"
+	"github.com/jeromelesaux/martine/gfx/filter"
 )
 
 type MartineUI struct {
@@ -39,6 +41,10 @@ type MartineUI struct {
 	palette           color.Palette
 	data              []byte
 	downgraded        *image.NRGBA
+	ditheringMatrix   [][]float32
+	ditheringType     constants.DitheringType
+	applyDithering    bool
+	resizeAlgo        imaging.ResampleFilter
 }
 
 func NewMartineUI() *MartineUI {
@@ -105,6 +111,11 @@ func (m *MartineUI) ApplyOneImage() {
 		context.Size.Height = 16
 		context.Size.Width = 16
 	}
+
+	if m.applyDithering {
+		context.DitheringMatrix = m.ditheringMatrix
+		context.DitheringType = m.ditheringType
+	}
 	pi := dialog.NewProgressInfinite("Computing", "Please wait.", m.window)
 	pi.Show()
 	out, downgraded, palette, _, err := gfx.ApplyOneImage(m.originalImage.Image, context, m.mode, color.Palette{}, uint8(m.mode))
@@ -142,6 +153,7 @@ func (m *MartineUI) newImageTransfertTab() fyne.CanvasObject {
 			}
 			canvasImg := canvas.NewImageFromImage(img)
 			m.originalImage = *canvas.NewImageFromImage(canvasImg.Image)
+			m.originalImage.FillMode = canvas.ImageFillContain
 			m.window.Canvas().Refresh(&m.originalImage)
 			m.window.Resize(m.window.Content().Size())
 		}, m.window)
@@ -184,6 +196,113 @@ func (m *MartineUI) newImageTransfertTab() fyne.CanvasObject {
 		}
 	})
 	winFormat.SetSelected("Normal")
+
+	resize := widget.NewSelect([]string{"NearestNeighbor",
+		"CatmullRom",
+		"Lanczos",
+		"Linear",
+		"Box",
+		"Hermite",
+		"BSpline",
+		"Hamming",
+		"Hann",
+		"Gaussian",
+		"Blackman",
+		"Bartlett",
+		"Welch",
+		"Cosine",
+		"MitchellNetravali",
+	}, func(s string) {
+		switch s {
+		case "NearestNeighbor":
+			m.resizeAlgo = imaging.NearestNeighbor
+		case "CatmullRom":
+			m.resizeAlgo = imaging.CatmullRom
+		case "Lanczos":
+			m.resizeAlgo = imaging.Lanczos
+		case "Linear":
+			m.resizeAlgo = imaging.Linear
+		case "Box":
+			m.resizeAlgo = imaging.Box
+		case "Hermite":
+			m.resizeAlgo = imaging.Hermite
+		case "BSpline":
+			m.resizeAlgo = imaging.BSpline
+		case "Hamming":
+			m.resizeAlgo = imaging.Hamming
+		case "Hann":
+			m.resizeAlgo = imaging.Hann
+		case "Gaussian":
+			m.resizeAlgo = imaging.Gaussian
+		case "Blackman":
+			m.resizeAlgo = imaging.Blackman
+		case "Bartlett":
+			m.resizeAlgo = imaging.Bartlett
+		case "Welch":
+			m.resizeAlgo = imaging.Welch
+		case "Cosine":
+			m.resizeAlgo = imaging.Cosine
+		case "MitchellNetravali":
+			m.resizeAlgo = imaging.MitchellNetravali
+		}
+	})
+
+	resize.SetSelected("NearestNeighbor")
+	resizeLabel := widget.NewLabel("Resize algorithm")
+
+	dithering := widget.NewSelect([]string{"FloydSteinberg",
+		"JarvisJudiceNinke",
+		"Stucki",
+		"Atkinson",
+		"Sierra",
+		"SierraLite",
+		"Sierra3",
+		"Bayer2",
+		"Bayer3",
+		"Bayer4",
+		"Bayer8",
+	}, func(s string) {
+		switch s {
+		case "FloydSteinberg":
+			m.ditheringMatrix = filter.FloydSteinberg
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "JarvisJudiceNinke":
+			m.ditheringMatrix = filter.JarvisJudiceNinke
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "Stucki":
+			m.ditheringMatrix = filter.Stucki
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "Atkinson":
+			m.ditheringMatrix = filter.Atkinson
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "Sierra":
+			m.ditheringMatrix = filter.Sierra
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "SierraLite":
+			m.ditheringMatrix = filter.SierraLite
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "Sierra3":
+			m.ditheringMatrix = filter.Sierra3
+			m.ditheringType = constants.ErrorDiffusionDither
+		case "Bayer2":
+			m.ditheringMatrix = filter.Bayer2
+			m.ditheringType = constants.OrderedDither
+		case "Bayer3":
+			m.ditheringMatrix = filter.Bayer3
+			m.ditheringType = constants.OrderedDither
+		case "Bayer4":
+			m.ditheringMatrix = filter.Bayer4
+			m.ditheringType = constants.OrderedDither
+		case "Bayer8":
+			m.ditheringMatrix = filter.Bayer8
+			m.ditheringType = constants.OrderedDither
+		}
+	})
+	dithering.SetSelected("FloydSteinberg")
+
+	enableDithering := widget.NewCheck("Enable dithering", func(b bool) {
+		m.applyDithering = b
+	})
 	isPlus := widget.NewCheck("CPC Plus", func(b bool) {
 		m.isCpcPlus = b
 	})
@@ -208,38 +327,54 @@ func (m *MartineUI) newImageTransfertTab() fyne.CanvasObject {
 		layout.NewGridLayoutWithColumns(2),
 		container.New(
 			layout.NewGridLayoutWithRows(2),
-			&m.originalImage,
-			&m.cpcImage,
+			container.NewScroll(
+				&m.originalImage),
+			container.NewScroll(
+				&m.cpcImage),
 		),
 		container.New(
-			layout.NewGridLayoutWithRows(2),
+			layout.NewGridLayoutWithRows(3),
 			container.New(
-				layout.NewGridLayoutWithColumns(3),
+				layout.NewHBoxLayout(),
 				openFileWidget,
 				applyButton,
 				exportButton,
 			),
 			container.New(
+				layout.NewHBoxLayout(),
+				isPlus,
+				winFormat,
+
+				container.New(
+					layout.NewGridLayoutWithRows(3),
+					container.New(
+						layout.NewGridLayoutWithRows(2),
+						modeLabel,
+						modes,
+					),
+					container.New(
+						layout.NewGridLayoutWithColumns(2),
+						widthLabel,
+						&m.width,
+					),
+					container.New(
+						layout.NewGridLayoutWithColumns(2),
+						heightLabel,
+						&m.height,
+					),
+				),
+			),
+			container.New(
 				layout.NewGridLayoutWithColumns(2),
 				container.New(
 					layout.NewGridLayoutWithColumns(2),
-					isPlus,
-					winFormat,
+					resizeLabel,
+					resize,
 				),
 				container.New(
 					layout.NewGridLayoutWithColumns(2),
-					modeLabel,
-					modes,
-				),
-				container.New(
-					layout.NewGridLayoutWithColumns(2),
-					widthLabel,
-					&m.width,
-				),
-				container.New(
-					layout.NewGridLayoutWithColumns(2),
-					heightLabel,
-					&m.height,
+					enableDithering,
+					dithering,
 				),
 			),
 		),
