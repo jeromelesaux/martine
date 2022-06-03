0c42fd1576db32b986a7624582759275cda6fa5e
Jerome Le Saux <jeromelesaux@gmail.com>
2022-06-03 15:02:24 +0200

    add: add sprite tab apply in UI.

---
+++ ui/martine-ui/menu/spriteMenu.go
@@ -0,0 +1,37 @@
+package menu
+
+import (
+	"image"
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"github.com/jeromelesaux/fyne-io/custom_widget"
+)
+
+var SpriteSize float32 = 80.
+
+type SpriteMenu struct {
+	IsHardSprite    bool
+	OriginalBoard   canvas.Image
+	OriginalPalette canvas.Image
+
+	Palette               color.Palette
+	SpritesData           [][]byte
+	CompileSprite         bool
+	IsCpcPlus             bool
+	OriginalImages        *custom_widget.ImageTable
+	SpritesCollection     []*image.NRGBA
+	SpriteNumberPerRow    int
+	SpriteNumberPerColumn int
+	Mode                  int
+}
+
+func NewSpriteMenu() *SpriteMenu {
+	return &SpriteMenu{
+		OriginalBoard:     canvas.Image{},
+		OriginalImages:    custom_widget.NewEmptyImageTable(fyne.NewSize(SpriteSize, SpriteSize)),
+		SpritesCollection: make([]*image.NRGBA, 0),
+		SpritesData:       make([][]byte, 0),
+	}
+}
---
+++ ui/martine-ui/spriteTab.go
@@ -0,0 +1,187 @@
+package ui
+
+import (
+	"fmt"
+	"image"
+	"os"
+	"strconv"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
+	"fyne.io/fyne/v2/widget"
+	"github.com/jeromelesaux/martine/constants"
+	"github.com/jeromelesaux/martine/convert"
+	"github.com/jeromelesaux/martine/gfx/sprite"
+	"github.com/jeromelesaux/martine/ui/martine-ui/menu"
+)
+
+func (m *MartineUI) ApplySprite(s *menu.SpriteMenu) {
+	var colorsAvailable int
+	switch s.Mode {
+	case 0:
+		colorsAvailable = constants.Mode0.ColorsAvailable
+	case 1:
+		colorsAvailable = constants.Mode1.ColorsAvailable
+	case 2:
+		colorsAvailable = constants.Mode2.ColorsAvailable
+	}
+	pal, _, err := convert.DowngradingPalette(s.OriginalBoard.Image.(*image.NRGBA), constants.Size{ColorsAvailable: colorsAvailable, Width: s.OriginalBoard.Image.Bounds().Max.X, Height: s.OriginalBoard.Image.Bounds().Max.Y}, s.IsCpcPlus)
+	if err != nil {
+		return
+	}
+	s.Palette = pal
+	raw, sprites, err := sprite.SplitBoardToSprite(s.OriginalBoard.Image, s.Palette, s.SpriteNumberPerRow, s.SpriteNumberPerColumn, uint8(s.Mode), s.IsHardSprite)
+	if err != nil {
+		return
+	}
+	s.SpritesCollection = sprites
+	s.SpritesData = raw
+
+	imagesCanvas := make([][]canvas.Image, s.SpriteNumberPerRow)
+	for i := 0; i < s.SpriteNumberPerRow; i++ {
+		imagesCanvas[i] = make([]canvas.Image, s.SpriteNumberPerColumn)
+	}
+
+	for x := 0; x < s.SpriteNumberPerRow; x++ {
+		for y := 0; y < s.SpriteNumberPerColumn; y++ {
+			imagesCanvas[x][y] = *canvas.NewImageFromImage(s.SpritesCollection[x+y])
+		}
+	}
+	s.OriginalImages.Update(&imagesCanvas, s.SpriteNumberPerRow, s.SpriteNumberPerColumn)
+	refreshUI.OnTapped()
+}
+
+func (m *MartineUI) newSpriteTab(s *menu.SpriteMenu) fyne.CanvasObject {
+
+	forceUIRefresh := widget.NewButtonWithIcon("Refresh UI", theme.ComputerIcon(), func() {
+		s := m.window.Content().Size()
+		s.Height += 10.
+		s.Width += 10.
+		m.window.Resize(s)
+		m.window.Resize(m.window.Content().Size())
+		m.window.Content().Refresh()
+	})
+	refreshUI = forceUIRefresh
+
+	openFileWidget := widget.NewButton("Image", func() {
+		d := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, m.window)
+				return
+			}
+			if reader == nil {
+				return
+			}
+
+			img, err := openImage(reader.URI().Path())
+			if err != nil {
+				dialog.ShowError(err, m.window)
+				return
+			}
+			canvasImg := canvas.NewImageFromImage(img)
+			s.OriginalBoard = *canvas.NewImageFromImage(canvasImg.Image)
+			s.OriginalBoard.FillMode = canvas.ImageFillContain
+			m.window.Canvas().Refresh(&s.OriginalBoard)
+			m.window.Resize(m.window.Content().Size())
+		}, m.window)
+		d.SetFilter(imagesFilesFilter)
+		d.Resize(dialogSize)
+		d.Show()
+	})
+
+	exportButton := widget.NewButtonWithIcon("Export", theme.DocumentSaveIcon(), func() {
+		m.exportDialog(m.imageExport, m.window)
+	})
+
+	applyButton := widget.NewButtonWithIcon("Apply", theme.VisibilityIcon(), func() {
+		fmt.Println("apply.")
+		m.ApplySprite(s)
+	})
+
+	openFileWidget.Icon = theme.FileImageIcon()
+
+	modes := widget.NewSelect([]string{"0", "1", "2"}, func(v string) {
+		mode, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error %s cannot be cast in int\n", v)
+		}
+		s.Mode = mode
+	})
+
+	modeLabel := widget.NewLabel("Mode:")
+
+	spriteNumberPerRowLabel := widget.NewLabel("Number of sprite per row")
+	spriteNumberPerColumnLabel := widget.NewLabel("Number of sprite per column")
+	spriteNumberPerColumnEntry := widget.NewEntry()
+	spriteNumberPerColumnEntry.OnChanged = func(v string) {
+		r, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error %s cannot be cast in int\n", v)
+			return
+		}
+		s.SpriteNumberPerColumn = r
+	}
+	spriteNumberPerRowEntry := widget.NewEntry()
+	spriteNumberPerRowEntry.OnChanged = func(v string) {
+		r, err := strconv.Atoi(v)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error %s cannot be cast in int\n", v)
+			return
+		}
+		s.SpriteNumberPerRow = r
+	}
+
+	isSpriteHard := widget.NewCheck("is sprite hard", func(b bool) {
+		s.IsHardSprite = b
+	})
+
+	IsCpcPlus := widget.NewCheck("is CPC Plus", func(b bool) {
+		s.IsCpcPlus = b
+	})
+
+	return container.New(
+		layout.NewGridLayoutWithColumns(2),
+		container.New(
+			layout.NewGridLayoutWithRows(2),
+			&s.OriginalBoard,
+			s.OriginalImages,
+		),
+		container.New(
+			layout.NewVBoxLayout(),
+			container.New(
+				layout.NewHBoxLayout(),
+				openFileWidget,
+				applyButton,
+				exportButton,
+				refreshUI,
+			),
+			container.New(
+				layout.NewHBoxLayout(),
+				modeLabel,
+				modes,
+			),
+			container.New(
+				layout.NewHBoxLayout(),
+				spriteNumberPerRowLabel,
+				spriteNumberPerColumnEntry,
+			),
+			container.New(
+				layout.NewHBoxLayout(),
+				spriteNumberPerColumnLabel,
+				spriteNumberPerRowEntry,
+			),
+			container.New(
+				layout.NewHBoxLayout(),
+				isSpriteHard,
+			),
+			container.New(
+				layout.NewHBoxLayout(),
+				IsCpcPlus,
+			),
+		),
+	)
+}
