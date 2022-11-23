95ac60d7b8849ee1a0bbc011baf62fa79f8ef6e8
Jerome Le Saux <jeromelesaux@gmail.com>
2022-11-23 22:12:44 +0100

    add: add test export go files.

---
+++ export/impdraw/overscan/go_test.go
@@ -0,0 +1,31 @@
+package overscan_test
+
+import (
+	"image/png"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/jeromelesaux/martine/config"
+	"github.com/jeromelesaux/martine/constants"
+	"github.com/jeromelesaux/martine/export/diskimage"
+	"github.com/jeromelesaux/martine/gfx"
+)
+
+func TestSaveGo(t *testing.T) {
+	fileInput := "../../../samples/lena-512.png"
+	f, _ := os.Open(fileInput)
+	defer f.Close()
+	img, _ := png.Decode(f)
+
+	cfg := config.NewMartineConfig(fileInput, "")
+	cfg.Overscan = true
+	cfg.ExportAsGoFile = true
+	cfg.Dsk = true
+	cfg.CpcPlus = true
+	cfg.Size = constants.NewSizeMode(0, true)
+	gfx.ApplyOneImageAndExport(img, cfg, cfg.InputPath, filepath.Dir(fileInput), 0, 0)
+	cfg.ExportAsGoFile = false
+	//gfx.ApplyOneImageAndExport(img, cfg, cfg.InputPath, filepath.Dir(fileInput), 0, 0)
+	diskimage.ImportInDsk(filepath.Dir(fileInput), cfg)
+}
