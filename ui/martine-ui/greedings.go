48b12424e6b04f17900d8c40ff6ab917d3a4d4fc
Jerome Le Saux <jeromelesaux@gmail.com>
2023-01-31 22:34:01 +0100

    add theme changer

--- ui/martine-ui/greedings.go
+++ ui/martine-ui/greedings.go
@@ -4,16 +4,18 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 func (m *MartineUI) newGreedings() fyne.CanvasObject {
 	return container.New(
-		layout.NewHBoxLayout(),
+		layout.NewVBoxLayout(),
 		widget.NewLabel(`Some greedings.
 		Thanks a lot to all the Impact members.
 		Ast, CMP, Demoniak, Kris and Drill
 		Specials thanks for support to : 
+		***        AST        ***
 		***      Tronic        ***
 		***        Siko          ***
 		*** Roudoudou ***
@@ -25,5 +27,34 @@ func (m *MartineUI) newGreedings() fyne.CanvasObject {
 		https://discord.com/channels/453480213032992768/454619697485447169 on discord
 		`),
 		layout.NewSpacer(),
+		container.New(
+			layout.NewHBoxLayout(),
+			widget.NewLabel("Change color scheme"),
+			widget.NewSelect([]string{"Black", "White"}, func(s string) {
+				a := fyne.CurrentApp()
+				switch s {
+				case "Black":
+					a.Settings().SetTheme(theme.DarkTheme())
+				case "White":
+					a.Settings().SetTheme(theme.LightTheme())
+				}
+			}),
+		),
+	/*	container.New(
+		layout.NewHBoxLayout(),
+		widget.NewLabel("Font"),
+		widget.NewButtonWithIcon("Increase", theme.ContentAddIcon(), func() {
+			a := fyne.CurrentApp()
+			v := a.Settings().Scale()
+			v += .2
+			os.Setenv("FYNE_SCALE", fmt.Sprintf("%.2f", v))
+		}),
+		widget.NewButtonWithIcon("Decrease", theme.ContentRemoveIcon(), func() {
+			a := fyne.CurrentApp()
+			v := a.Settings().Scale()
+			v -= .2
+			os.Setenv("FYNE_SCALE", fmt.Sprintf("%.2f", v))
+		}),
+	),*/
 	)
 }
